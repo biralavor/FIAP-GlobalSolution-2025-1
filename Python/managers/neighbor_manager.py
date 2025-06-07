265be7555e775d4e003e2e74e35fac7ef270a075
Bira Lavor <umeneses@student.42sp.org.br>
2025-06-07 11:23:33 -0300

    feat #16

---
+++ Python/managers/neighbor_manager.py
@@ -0,0 +1,17 @@
+from utils.finders import ask_users_location
+from utils.finders import neighbor_risk_finder
+from utils.finders import neighborhood_finder
+from utils.validations import does_str_has_special_char
+from generators.alerts import alert_generator
+
+def neighborhood_manager(user_type: str, data_with_rainfall: dict) -> int:
+    user_location = ask_users_location(user_type)
+    while does_str_has_special_char(user_location):
+        user_location = ask_users_location(user_type)
+    neighbor = neighborhood_finder(user_location, data_with_rainfall)
+    if neighbor:
+        risk_value = neighbor_risk_finder(neighbor, data_with_rainfall)
+        alert_generator(risk_value)
+    if risk_value == 4:
+        exit(0)
+    return risk_value

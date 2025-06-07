1cc67b7eef69c4266923de883781901c535b61bc
Bira Lavor <umeneses@student.42sp.org.br>
2025-06-07 11:23:33 -0300

    feat #16

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,18 @@
 import asyncio
 
+from utils.printers import sirena_title_printer
 from utils.printers import main_menu_printer
 from utils.printers import citizen_menu_printer
 from utils.printers import invalid_choice
 from utils.printers import exiting_printer
+from utils.printers import clear_screen
 from utils.validations import ask_valid_nbr
-from utils.finders import neighborhood_finder
-from utils.finders import ask_users_location
-from utils.finders import neighbor_risk_finder
 from utils.loaders import csv_loader
 from utils.loaders import csv_parser
-from utils.validations import does_str_has_special_char
-
-from generators.alerts import alert_generator
+from managers.neighbor_manager import neighborhood_manager
 from generators.simulators import rainfall_init
 from generators.simulators import extreme_rainfall_risk_simulator
+
 
 SP_NEIGHBORHOODS = "database-files/distritos-sp.csv"
 
@@ -24,45 +22,38 @@ async def main():
     parsed_data = csv_parser(data)
     data_with_rainfall = rainfall_init(parsed_data)
     data_with_rainfall = await extreme_rainfall_risk_simulator(data_with_rainfall)
+    clear_screen()
+    sirena_title_printer()
     while True:
         main_menu_printer()
         choice = ask_valid_nbr()
         match choice:
             case 1:
-                while True:
-                    user_location = ask_users_location("CITIZEN")
-                    while does_str_has_special_char(user_location):
-                        user_location = ask_users_location("CITIZEN")
-                    print(f"Searching for neighborhood = {user_location}")
-                    neighbor = neighborhood_finder(user_location, data_with_rainfall)
-                    if neighbor:
-                        risk_value = neighbor_risk_finder(neighbor, data_with_rainfall)
-                        alert_generator(risk_value)
-                        break
-                citizen_menu_printer()
-                while True:
-                    citizen_choice = ask_valid_nbr()
-                    match citizen_choice:
-                        case 1:
-                            print("Reporting Incident...")
-                        case 2:
-                            print("Checking if you are in danger...")
-                        case 3:
-                            break
-                        case _:
-                            invalid_choice()
-                            citizen_menu_printer()
+                user_type = "CITIZEN"
+                risk_value = neighborhood_manager(user_type, data_with_rainfall)
+                if risk_value < 4:
+                    citizen_menu_printer()
+                    while True:
+                        citizen_choice = ask_valid_nbr()
+                        match citizen_choice:
+                            case 1:
+                                print("Reporting Incident...")
+                            case 2:
+                                print("Checking if you are in danger...")
+                            case 3:
+                                break
+                            case _:
+                                invalid_choice()
+                                citizen_menu_printer()
             case 2:
-                print("Loading City Patrol Agent System...")
-                user_location = ask_users_location("AGENT")
-                while does_str_has_special_char(user_location):
-                    user_location = ask_users_location("AGENT")
-                neighborhood_finder(user_location, data_with_rainfall)
+                user_type = "AGENT"
+                # neighborhood_manager(user_type, data_with_rainfall)
             case 3:
                 exiting_printer()
             case _:
                 invalid_choice()
                 citizen_menu_printer()
+        clear_screen()
 
 if __name__ == '__main__':
     asyncio.run(main())

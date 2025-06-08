1f1985f9175479c628482e82baac5a26928bc44d
Bira Lavor <umeneses@student.42sp.org.br>
2025-06-08 12:20:51 -0300

    refactor javadocs

--- Java/javadocs/package-search-index.js
+++ Java/javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.data4lives.sirena"},{"l":"br.com.data4lives.sirena.autoridades"},{"l":"br.com.data4lives.sirena.reports"}];updateSearchResults();
